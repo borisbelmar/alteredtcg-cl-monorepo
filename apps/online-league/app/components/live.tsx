9c3bcd9f71d645f10f0307d3f0c985f99054a4a8
Boris Belmar <borisbelmarm@gmail.com>
2024-11-17 17:05:51 -0300

    Liga fixes

--- apps/online-league/app/components/live.tsx
+++ apps/online-league/app/components/live.tsx
@@ -1,10 +1,35 @@
+'use client'
+
+import { useEffect, useRef } from "react"
+
 export default function Live () {
+  const ref = useRef<HTMLDivElement>(null)
+  const iframeRef = useRef<HTMLIFrameElement>(null)
+
+  useEffect(() => {
+    const onResize = () => {
+      if (iframeRef.current) {
+        iframeRef.current.width = `${ref.current?.clientWidth?.toString()}px`,
+        iframeRef.current.height = `${ref.current?.clientHeight?.toString()}px`
+      }
+    }
+    onResize()
+    if (iframeRef.current) {
+      window.addEventListener('resize', onResize)
+    }
+    return () => {
+      window.removeEventListener('resize',  onResize)
+    }
+  }, [])
+
   return (
-    <div className="flex flex-col items-center justify-center px-8 py-16">
-      <h2 className="text-2xl font-bold mb-8">
+    <div className="flex flex-col items-center justify-center px-8 py-16 w-full">
+      <h2 className="text-2xl font-bold mb-8 text-center">
         Streaming Oficial de la Liga
       </h2>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/IibdwLyohzw?si=IUGF293qWxYtmTHu" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe>
+      <div ref={ref} className="relative w-full flex-1 max-w-2xl" style={{ aspectRatio: '16/9' }}>
+        <iframe ref={iframeRef} className="abosolute inset-0" src="https://www.youtube.com/embed/IibdwLyohzw?si=IUGF293qWxYtmTHu" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" />
+      </div>
     </div>
   )
 }
