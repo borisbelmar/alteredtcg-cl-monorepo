271fda2286d9553d84e09dcd5b72fe769c393565
Boris Belmar <borisbelmarm@gmail.com>
2024-11-17 18:02:24 -0300

    Hero

--- apps/online-league/app/components/hero.tsx
+++ apps/online-league/app/components/hero.tsx
@@ -15,7 +15,7 @@ export default function Hero () {
       <div id="gradient" className="absolute w-full h-full top-0 left-0" />
       <CornerAltered className="absolute bottom-0 left-0 w-[40vw] max-w-64 m-4 [transform:rotateZ(180deg)]" />
       <CornerAltered className="absolute bottom-0 right-0 w-[40vw] max-w-64 m-4 [transform:rotateZ(180deg)_rotateY(180deg)]" />
-      <div className="flex flex-col md:flex-row justify-center items-center px-16 z-10 max-w-6xl mx-auto text-center md:text-left gap-8 mb-8">
+      <div className="flex flex-col md:flex-row justify-center items-center px-16 z-10 max-w-6xl mx-auto text-center md:text-left gap-8 mb-16">
         <Image src="/logo-alterados.png" alt="Circuito Altered TCG Chile" width={250} height={250} className="shrink-0" />
         <div>
           <h1 className="text-xl md:text-4xl sm:text-3xl font-bold text-white mb-2">
@@ -24,9 +24,17 @@ export default function Hero () {
           <h2 className="text-xs sm:text-base text-white">
             Todos los domingos a las 20:00 hrs en BGA.
           </h2>
-          <p className="text-sm sm:text-lg text-white mt-4">
+          <p className="text-sm sm:text-lg text-white my-8">
             La liga online de Altered TCG Chile es un torneo semanal <strong>gratuito</strong> que se juega a través de la plataforma <strong>BGA</strong> que busca fomentar la competencia y el juego en la región.
           </p>
+          <a
+            className="bg-sky-500 text-white font-bold px-6 py-4 rounded-lg hover:bg-sky-600 transition-colors whitespace-nowrap text-center"
+            href="https://boardgamearena.com/tournament?id=340269"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Inscribirme en BGA a Liga CL #5
+          </a>
         </div>
       </div>
     </div>
