c337c8a9693dc75b1794334e7e6f980b8dab131f
Boris Belmar <borisbelmarm@gmail.com>
2024-11-24 23:22:51 -0300

    feat: Update Hero and NextDates components with new event details and modify link structure

--- apps/online-league/app/components/nextDates.tsx
+++ apps/online-league/app/components/nextDates.tsx
@@ -3,6 +3,13 @@ import EventItem from "./eventItem";
 
 const pastEvents = [
   {
+    image: "/liga-fecha-2.webp",
+    title: "Liga CL Fecha #6",
+    date: "Domingo 24 de Noviembre",
+    time: "20:00 hrs",
+    tournamentLink: "http://boardgamearena.com/tournament?id=341463"
+  },
+  {
     image: "/liga-fecha-1.webp",
     title: "Liga CL Fecha #5",
     date: "Domingo 17 de Noviembre",
@@ -15,7 +22,7 @@ const pastEvents = [
 export default function NextDates () {
   return (
     <div className="flex flex-col gap-2 max-w-6xl w-full mx-auto px-8 py-16">
-      <h2 className="text-2xl font-bold">Próximas Fechas</h2>
+      {/* <h2 className="text-2xl font-bold">Próximas Fechas</h2>
       <Image src="/separator-half.png" alt="Separator" className="w-52 h-0.5 mb-4" width={250} height={5} />
       <EventItem
         image="/liga-fecha-2.webp"
@@ -24,7 +31,7 @@ export default function NextDates () {
         time="20:00 hrs"
         buttonLabel="Inscríbete ahora en BGA"
         tournamentLink="http://boardgamearena.com/tournament?id=341463&token=4Z4FYJAYCNCNu4rL5TEwiyWx28DjdeCX"
-      />
+      /> */}
       <h2 className="text-2xl font-bold mt-8">Fechas Pasadas</h2>
       <Image src="/separator-half.png" alt="Separator" className="w-52 h-0.5 mb-4" width={250} height={5} />
       <div className="flex flex-col gap-4">
